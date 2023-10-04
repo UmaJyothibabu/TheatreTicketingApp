363548b015e05acde0d246720066cde0e03f9a4f
Uma K J <umakj1995@gmail.com>
2023-10-04 19:23:27 +0530

    Aws

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = new express();
 
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
 const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
@@ -36,6 +39,68 @@ const connectDB = async () => {
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "/build/index.html"));
+});
+
+app.get("*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  try {
+    let s3File = await s3
+      .getObject({
+        Bucket: process.env.BUCKET,
+        Key: filename,
+      })
+      .promise();
+
+    res.set("Content-type", s3File.ContentType);
+    res.send(s3File.Body.toString()).end();
+  } catch (error) {
+    if (error.code === "NoSuchKey") {
+      console.log(`No such key ${filename}`);
+      res.sendStatus(404).end();
+    } else {
+      console.log(error);
+      res.sendStatus(500).end();
+    }
+  }
+});
+
+app.put("*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  console.log(typeof req.body);
+
+  await s3
+    .putObject({
+      Body: JSON.stringify(req.body),
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    })
+    .promise();
+
+  res.set("Content-type", "text/plain");
+  res.send("ok").end();
+});
+
+// curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
+app.delete("*", async (req, res) => {
+  let filename = req.path.slice(1);
+
+  await s3
+    .deleteObject({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    })
+    .promise();
+
+  res.set("Content-type", "text/plain");
+  res.send("ok").end();
+});
+
+// /////////////////////////////////////////////////////////////////////////////
+// Catch all handler for all other request.
+app.use("*", (req, res) => {
+  res.sendStatus(404).end();
 });
 
 connectDB().then(() => {
