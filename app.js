0d5da6dfc55330dbcfd9414c7b4252cedce4c1ed
Uma K J <umakj1995@gmail.com>
2023-10-04 20:12:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const app = new express();
+const app = express(); // No need for 'new' keyword
 
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} = require("@aws-sdk/client-s3"); // Import AWS SDK v3 S3Client and commands
+const s3 = new S3Client(); // Create an S3 client instance
 
 const cors = require("cors");
 const morgan = require("morgan");
@@ -23,16 +28,16 @@ const customerApi = require("./Routes/customer");
 const adminApi = require("./Routes/admin");
 app.use("/uploads", express.static("uploads"));
 
-app.use("/api", accountApi); //routes for login and sign up
-app.use("/api", adminApi); //routes handling admin activities
-app.use("/api", customerApi); //routes handling customer activities
+app.use("/api", accountApi); // Routes for login and sign up
+app.use("/api", adminApi); // Routes handling admin activities
+app.use("/api", customerApi); // Routes handling customer activities
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.mongodb_url);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.error(error); // Use console.error for errors
     process.exit(1);
   }
 };
@@ -45,21 +50,21 @@ app.get("*", async (req, res) => {
   let filename = req.path.slice(1);
 
   try {
-    let s3File = await s3
-      .getObject({
+    const s3File = await s3.send(
+      new GetObjectCommand({
         Bucket: process.env.BUCKET,
         Key: filename,
       })
-      .promise();
+    );
 
     res.set("Content-type", s3File.ContentType);
-    res.send(s3File.Body.toString()).end();
+    res.send(Buffer.from(s3File.Body)).end();
   } catch (error) {
-    if (error.code === "NoSuchKey") {
+    if (error.name === "NoSuchKey") {
       console.log(`No such key ${filename}`);
       res.sendStatus(404).end();
     } else {
-      console.log(error);
+      console.error(error); // Use console.error for errors
       res.sendStatus(500).end();
     }
   }
@@ -70,148 +75,33 @@ app.put("*", async (req, res) => {
 
   console.log(typeof req.body);
 
-  await s3
-    .putObject({
+  await s3.send(
+    new PutObjectCommand({
       Body: JSON.stringify(req.body),
       Bucket: process.env.BUCKET,
       Key: filename,
     })
-    .promise();
+  );
 
   res.set("Content-type", "text/plain");
   res.send("ok").end();
 });
 
-// curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
 app.delete("*", async (req, res) => {
   let filename = req.path.slice(1);
 
-  await s3
-    .deleteObject({
+  await s3.send(
+    new DeleteObjectCommand({
       Bucket: process.env.BUCKET,
       Key: filename,
     })
-    .promise();
-  const express = require("express");
-  const app = express(); // No need for 'new' keyword
-
-  const {
-    S3Client,
-    GetObjectCommand,
-    PutObjectCommand,
-    DeleteObjectCommand,
-  } = require("@aws-sdk/client-s3"); // Import AWS SDK v3 S3Client and commands
-  const s3 = new S3Client(); // Create an S3 client instance
-
-  const cors = require("cors");
-  const morgan = require("morgan");
-  const path = require("path");
-  require("dotenv").config();
-  const mongoose = require("mongoose");
-
-  app.use(express.static(path.join(__dirname, "/build")));
-  app.use(cors());
-  app.use(morgan("dev"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-
-  const PORT = process.env.PORT;
-
-  const accountApi = require("./Routes/users");
-  const customerApi = require("./Routes/customer");
-  const adminApi = require("./Routes/admin");
-  app.use("/uploads", express.static("uploads"));
-
-  app.use("/api", accountApi); // Routes for login and sign up
-  app.use("/api", adminApi); // Routes handling admin activities
-  app.use("/api", customerApi); // Routes handling customer activities
-
-  const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.mongodb_url);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-      console.error(error); // Use console.error for errors
-      process.exit(1);
-    }
-  };
-
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/build/index.html"));
-  });
-
-  app.get("*", async (req, res) => {
-    let filename = req.path.slice(1);
-
-    try {
-      const s3File = await s3.send(
-        new GetObjectCommand({
-          Bucket: process.env.BUCKET,
-          Key: filename,
-        })
-      );
-
-      res.set("Content-type", s3File.ContentType);
-      res.send(Buffer.from(s3File.Body)).end();
-    } catch (error) {
-      if (error.name === "NoSuchKey") {
-        console.log(`No such key ${filename}`);
-        res.sendStatus(404).end();
-      } else {
-        console.error(error); // Use console.error for errors
-        res.sendStatus(500).end();
-      }
-    }
-  });
-
-  app.put("*", async (req, res) => {
-    let filename = req.path.slice(1);
-
-    console.log(typeof req.body);
-
-    await s3.send(
-      new PutObjectCommand({
-        Body: JSON.stringify(req.body),
-        Bucket: process.env.BUCKET,
-        Key: filename,
-      })
-    );
-
-    res.set("Content-type", "text/plain");
-    res.send("ok").end();
-  });
-
-  app.delete("*", async (req, res) => {
-    let filename = req.path.slice(1);
-
-    await s3.send(
-      new DeleteObjectCommand({
-        Bucket: process.env.BUCKET,
-        Key: filename,
-      })
-    );
-
-    res.set("Content-type", "text/plain");
-    res.send("ok").end();
-  });
-
-  // Catch all handler for all other requests.
-  app.use("*", (req, res) => {
-    res.sendStatus(404).end();
-  });
-
-  connectDB().then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running at port ${PORT}`);
-    });
-  });
+  );
 
   res.set("Content-type", "text/plain");
   res.send("ok").end();
 });
 
-// /////////////////////////////////////////////////////////////////////////////
-// Catch all handler for all other request.
+// Catch all handler for all other requests.
 app.use("*", (req, res) => {
   res.sendStatus(404).end();
 });
